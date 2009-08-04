e214226b4c197db122f0aca44e0c6c19fe8005f9
Nick Fisher <fish@FishBook.local>
2009-08-04 01:24:53 -0400

    now stores meta data locally and only uploads new data, lots of refactoring

--- sync.py
+++ sync.py
@@ -7,9 +7,14 @@ from datetime import datetime
 server = couchdb.Server('http://localhost:5984/')
 db = server['noteish']
 
+import pickle
+import hashlib
+
 import logging
 log = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
+
+meta_store = {}
 
 class CouchNote(schema.Document):
     summary = schema.TextField()
@@ -20,63 +25,78 @@ class CouchNote(schema.Document):
     implements = schema.DictField(
         schema.Schema.build(couchnote = schema.BooleanField(default = True)))
 
-def save_note(doc_dict):
-    if doc_dict.has_key('id'):
-        note = CouchNote.load(db, doc_dict['id'])
-        note.detail = doc_dict['detail']
-        note.summary = doc_dict['summary']
-        # if note.rev != doc_dict['rev']: ask_user()
+def save_note(meta, detail):
+    if meta.has_key('id'):
+        note = CouchNote.load(db, meta['id'])
+        note.summary = meta['summary']
+        note.file_path = meta['file_path']
+        log.info('Updating note: %s'%meta['file_path'])
+        # if note.rev != meta['rev']: ask_user()
     else:
         # warn about duplicat summary
-        # if len(db.view('couchapp/summaries', key=doc_dict['summary'])) > 0:
+        # if len(db.view('couchapp/summaries', key=meta['summary'])) > 0:
         # ask_user()
-        log.debug('Making note: %s'%str(doc_dict))
+        log.info('New note: %s'%meta['file_path'])
         try:
-            note = CouchNote(**doc_dict)
+            note = CouchNote(detail = detail, **meta)
         except UnicodeDecodeError:
-            log.error('Could not import %s'%doc_dict['summary'])
+            log.error('Could not import %s'%meta['summary'])
             return
     note.store(db)
+    meta['id'] = note.id
+    meta['rev'] = note.rev
+    set_meta(meta['file_path'], meta)
+
+def load_store():
+    global meta_store
+    if os.path.isfile('meta.cache'):
+        meta_store = pickle.load(open('meta.cache'))
+    else:
+        open('meta.cache','w').close()
+
+def save_store():
+    global meta_store
+    pickle.dump(meta_store, open('meta.cache','w'))
 
 def ask_user(question):
     # UI abstraction, return true or false
     pass
 
-def get_meta(source_file_path):
-    # Meta data store abstraction
-    # Load dict from sqlite
-    pass
+def set_meta(file_path, meta):
+    global meta_store
+    # Save the file's couch meta data
+    meta_store[file_path] = meta
 
-def import_file(path, base_path):
-    log.info('Importing %s'%path)
-    doc_dict = {}
-    doc_dict['file_path'] = path[len(base_path):]
-    doc_dict['summary'] = os.path.basename(path[:path.rfind('.')])
-    content = open(path).read()
-    doc_dict['detail'] = content
-    save_note(doc_dict)
+def get_meta(path):
+    global meta_store
+    if meta_store.has_key(path):
+        return meta_store[path]
+    else:
+        meta = {}
+        meta['file_path'] = path
+        meta['summary'] = os.path.basename(path[:path.rfind('.')])
+        return meta
 
-def import_dir(scan_dir):
+def sync_dir(scan_dir):
     for root, dirs, files in os.walk(scan_dir):
         log.info('Scanning %s'%root)
         for file in files:
-            ext = file[file.rfind('.')+1:]
-            if ext == 'txt':
-                import_file(os.path.join(root,file), base_path=scan_dir)
+            ext = os.path.splitext(file)[1]
+            if ext == '.txt':
+                full_path = os.path.join(root,file)
+                rel_path = full_path[len(scan_dir):].lstrip('/')
+                content = open(full_path).read()
+                md5 = hashlib.md5(content).hexdigest()
+                meta = get_meta(rel_path)
+                if md5 != meta.get('md5',''):
+                    meta['md5'] = md5
+                    save_note(meta, content)
 
 def main():
-    #note = 'ramma lamma happy feet 2'
-    #meta = {'summary':'my first nope', 'id':'43e9c9f150a28b3e4be73203f84a10be'}
-    #meta['detail'] = note
-    #save_note(meta)
-# note_ext = extract_ext(filename)
-# note_meta['format'] = format_from_ext(note_ext)
+    load_store()
     scan_dir = '/Users/fish/Documents/Notes'
-    import_dir(scan_dir)
-
+    sync_dir(scan_dir)
+    save_store()
 
 if __name__ == '__main__':
     main()
-# for note in db
-# compair local copy to db copy
-# Locally in the file? In sqlite?

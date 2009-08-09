beace3e25cf518af3c9b3f882358e1cbdd4a6a4d
Nick Fisher <fish@FishBook.local>
2009-08-09 19:43:20 -0400

    Added option parsing and cleaned up main

--- sync.py
+++ sync.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-import os.path
+import os
+import sys
 import couchdb
 server = couchdb.Server('http://localhost:5984/')
 db = server['noteish']
@@ -10,27 +11,32 @@ import hashlib
 from store import MetaStore
 from model import CouchNote
 
+from optparse import OptionParser
+
 import logging
 log = logging.getLogger()
 logging.basicConfig(level=logging.DEBUG)
 
 scan_dir = './test_notes'
 
-def upload_note(note_id, store):
-    meta = store[note_id]
-    log.info('Updating note: %s'%meta['file_path'])
-    note = CouchNote.load(db, meta['id'])
-    meta['rev'] = note.rev
-    note.detail = open(meta['file_path']).read()
-    note.store(db)
-    store.update_metas((note, meta['file_path']))
+def upload_notes(note_ids, store):
+    updates = []
+    for note_id in note_ids:
+        meta = store[note_id]
+        log.info('Updating note: %s'%meta['file_path'])
+        note = CouchNote.load(db, meta['id'])
+        note.detail = open(os.path.join(scan_dir, meta['file_path'])).read()
+        note.store(db)
+        updates.append((note, meta['file_path']))
+    store.update_metas(updates)
 
-def new_note(file_path):
-    # split path
-    # make summary and file_path
-    # make and save note
-    # put in store
-    pass
+def new_note(file_path, store):
+    file_name = os.path.split(file_path)[1]
+    summary = os.path.splitext(file_name)[0]
+    detail = open().read()
+    note = CouchNote(summary=summary, detail=detail)
+    note.store(db)
+    store.update_metas((note, file_path))
 
 def get_new_local(scan_dir):
     local_new = []
@@ -93,28 +99,54 @@ def get_local_changed(store):
         del store[killed]
     return local_changes
 
+def do_parser():
+    usage = '''usage: %prog command [options]
+    
+Commands:
+  import - imports unkown txt files as new couch notes
+  sync - syncs non-conflicting changes with couch
+  download - downloads all couch notes'''
+    parser = OptionParser(usage=usage)
+    parser.add_option('-v', '--verbose', action='store', dest='level',
+                      help='Output INFO')
+    options, args = parser.parse_args(sys.argv)
+    if len(args) < 2:
+        parser.print_help()
+        sys.exit('No command specified specified')
+    if args[1] not in ['import', 'sync', 'download']:
+        parser.print_help()
+        sys.exit('Bad command: %s'%args[1])
+    return options, args
+
 def main():
+    options, args = do_parser()
     store = MetaStore()
-    local_changes = get_local_changed(store)
-    remote_changes = get_couch_changed(store)
-    #remote_changes = get_couch_new(store)
-    conflicts = []
-    upload = []
-    download = []
-    for path in local_changes:
-        if path not in remote_changes:
-            upload.append(path)
-        else:
-            conflicts.append(path)
-    for path in remote_changes:
-        if path not in conflicts:
-            download.append(path)
+    if args[1] == 'sync':
+        local_changes = get_local_changed(store)
+        remote_changes = get_couch_changed(store)
+        #remote_changes = get_couch_new(store)
+        conflicts = []
+        upload = []
+        download = []
+        for path in local_changes:
+            if path not in remote_changes:
+                upload.append(path)
+            else:
+                conflicts.append(path)
+        for path in remote_changes:
+            if path not in conflicts:
+                download.append(path)
 
-    print 'Uploading: %s'%upload
-    print 'Downloading: %s'%download
-    print 'Conlifting: %s'%conflicts
+        if 0:
+            print 'Uploading: %s'%upload
+            print 'Downloading: %s'%download
+            print 'Conlifting: %s'%conflicts
 
-    download_notes(download, store)
+        download_notes(download, store)
+        upload_notes(upload, store)
+        for conflict in conflicts:
+            meta = store[conflict]
+            log.warn('Conflicting changes to: %(file_path)s [%(id)s]'%meta)
     store.save()
 
 if __name__ == '__main__':
